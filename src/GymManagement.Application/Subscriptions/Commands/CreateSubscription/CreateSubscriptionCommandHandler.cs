8fa46a3b6625fa26eb970907f51730a9f53a8367
TJ Lopez <wtjlopez99@gmail.com>
2024-04-17 16:00:16 -0400

    Added the unit of work interface. It will not work yet as we do not have the implementation of it just yet

--- src/GymManagement.Application/Subscriptions/Commands/CreateSubscription/CreateSubscriptionCommandHandler.cs
+++ src/GymManagement.Application/Subscriptions/Commands/CreateSubscription/CreateSubscriptionCommandHandler.cs
@@ -8,10 +8,12 @@ namespace GymManagement.Application.Subscriptions.Commands.CreateSubscription;
 public class CreateSubscriptionCommandHandler : IRequestHandler<CreateSubscriptionCommand, ErrorOr<Subscription>>
 {
     private readonly ISubscriptionsRepository _subscriptionsRepository;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public CreateSubscriptionCommandHandler(ISubscriptionsRepository subscriptionsRepository)
+    public CreateSubscriptionCommandHandler(ISubscriptionsRepository subscriptionsRepository, IUnitOfWork unitOfWork)
     {
         _subscriptionsRepository = subscriptionsRepository;
+        _unitOfWork = unitOfWork;
     }
 
     public async Task<ErrorOr<Subscription>> Handle(CreateSubscriptionCommand request, CancellationToken cancellationToken)
@@ -22,8 +24,9 @@ public class CreateSubscriptionCommandHandler : IRequestHandler<CreateSubscripti
             Id = Guid.NewGuid()
         };
         // Add it to the database
-        _subscriptionsRepository.AddSubscription(subscription);
-
+        await _subscriptionsRepository.AddSubscriptionAsync(subscription);
+        await _unitOfWork.CommitChangesAsync();
+        
         // return the subscription
         return subscription;
     }
