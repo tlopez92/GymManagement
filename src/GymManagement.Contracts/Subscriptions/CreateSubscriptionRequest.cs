eea2dd98e3ef024d8986d5bcb166a56c9e6ba31e
TJ Lopez <wtjlopez99@gmail.com>
2024-04-17 11:52:15 -0400

    Created the first controller and updated the program.cs file to match what we currently have

--- src/GymManagement.Contracts/Subscriptions/CreateSubscriptionRequest.cs
+++ src/GymManagement.Contracts/Subscriptions/CreateSubscriptionRequest.cs
@@ -1,3 +1,3 @@
 namespace GymManagement.Contracts.Subscriptions;
 
-public record CreateSubscriptionRequest(SubscriptionType SubscriptionType, Guid Admin);
+public record CreateSubscriptionRequest(SubscriptionType SubscriptionType, Guid AdminId);
