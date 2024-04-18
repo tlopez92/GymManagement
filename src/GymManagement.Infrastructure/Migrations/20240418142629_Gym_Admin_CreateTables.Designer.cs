d3cfbb05da58ac4350b5b913bff0fd0e21dcd972
TJ Lopez <wtjlopez99@gmail.com>
2024-04-18 10:27:31 -0400

    Migration file added for new tables and schema change

--- src/GymManagement.Infrastructure/Migrations/20240418142629_Gym_Admin_CreateTables.Designer.cs
+++ src/GymManagement.Infrastructure/Migrations/20240418142629_Gym_Admin_CreateTables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GymManagement.Infrastructure.Common.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GymManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(GymManagementDbContext))]
-    partial class GymManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240418142629_Gym_Admin_CreateTables")]
+    partial class Gym_Admin_CreateTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.4");
---
+++ src/GymManagement.Infrastructure/Migrations/20240418142629_Gym_Admin_CreateTables.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GymManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Gym_Admin_CreateTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GymIds",
+                table: "Subscriptions",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxGyms",
+                table: "Subscriptions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    SubscriptionId = table.Column<Guid>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Gyms",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    SubscriptionId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    MaxRooms = table.Column<int>(type: "INTEGER", nullable: false),
+                    RoomIds = table.Column<string>(type: "TEXT", nullable: false),
+                    TrainerIds = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gyms", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Admins",
+                columns: new[] { "Id", "SubscriptionId" },
+                values: new object[] { new Guid("2150e333-8fdc-42a3-9474-1a3956d46de8"), null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admins");
+
+            migrationBuilder.DropTable(
+                name: "Gyms");
+
+            migrationBuilder.DropColumn(
+                name: "GymIds",
+                table: "Subscriptions");
+
+            migrationBuilder.DropColumn(
+                name: "MaxGyms",
+                table: "Subscriptions");
+        }
+    }
+}
