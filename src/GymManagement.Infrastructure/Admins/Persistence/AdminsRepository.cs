19837683efbbf800086fa1ad1840b2f5da80b915
TJ Lopez <wtjlopez99@gmail.com>
2024-04-18 10:25:33 -0400

    Added the AdminsRepository

---
+++ src/GymManagement.Infrastructure/Admins/Persistence/AdminsRepository.cs
@@ -0,0 +1,27 @@
+using GymManagement.Application.Common.Interfaces;
+using GymManagement.Domain.Admins;
+using GymManagement.Infrastructure.Common.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManagement.Infrastructure.Admins.Persistence;
+
+public class AdminsRepository : IAdminsRepository
+{
+    private readonly GymManagementDbContext _dbContext;
+    
+    public AdminsRepository(GymManagementDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task<Admin?> GetByIdAsync(Guid adminId)
+    {
+        return await _dbContext.Admins.FirstOrDefaultAsync(a => a.Id == adminId);
+    }
+
+    public Task UpdateAsync(Admin admin)
+    {
+        _dbContext.Admins.Update(admin);
+        return Task.CompletedTask;
+    }
+}
--- src/GymManagement.Infrastructure/DependencyInjection.cs
+++ src/GymManagement.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
 using GymManagement.Application.Common.Interfaces;
+using GymManagement.Infrastructure.Admins.Persistence;
 using GymManagement.Infrastructure.Common.Persistence;
+using GymManagement.Infrastructure.Gyms.Persistence;
 using GymManagement.Infrastructure.Subscriptions.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,6 +17,8 @@ public static class DependencyInjection
             options.UseSqlite("Data Source=GymManagement.db");
         });
         
+        services.AddScoped<IAdminsRepository, AdminsRepository>();
+        services.AddScoped<IGymsRepository, GymsRepository>();
         services.AddScoped<ISubscriptionsRepository, SubscriptionsRepository>();
         services.AddScoped<IUnitOfWork>(serviceProvider => serviceProvider.GetRequiredService<GymManagementDbContext>());
         return services;
