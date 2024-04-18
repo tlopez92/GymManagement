99af4873de309652667e7d16fff750eeda58aba9
TJ Lopez <wtjlopez99@gmail.com>
2024-04-18 08:47:06 -0400

    Created the CreateGymCommand and handler functions

---
+++ src/GymManagement.Application/Gyms/Queries/CreateGym/CreateGymCommandHandler.cs
@@ -0,0 +1,51 @@
+using ErrorOr;
+using GymManagement.Application.Common.Interfaces;
+using GymManagement.Domain.Gyms;
+using MediatR;
+
+namespace GymManagement.Application.Gyms.Queries.CreateGym;
+
+public class CreateGymCommandHandler : IRequestHandler<CreateGymCommand, ErrorOr<Gym>>
+{
+    private readonly ISubscriptionsRepository _subscriptionsRepository;
+    private readonly IGymsRepository _gymsRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    
+    public CreateGymCommandHandler(
+        ISubscriptionsRepository subscriptionsRepository,
+        IGymsRepository gymsRepository,
+        IUnitOfWork unitOfWork)
+    {
+        _subscriptionsRepository = subscriptionsRepository;
+        _gymsRepository = gymsRepository;
+        _unitOfWork = unitOfWork;
+    }
+    
+    public async Task<ErrorOr<Gym>> Handle(CreateGymCommand request, CancellationToken cancellationToken)
+    {
+        var subscription = await _subscriptionsRepository.GetByIdAsync(request.SubscriptionId);
+        
+        if (subscription is null)
+        {
+            return Error.NotFound(description: "Subscription not found");
+        }
+        
+        var gym = new Gym(
+            name: request.Name,
+            maxRooms: subscription.GetMaxRooms(),
+            subscriptionId: subscription.Id);
+        
+        var addGymResult = subscription.AddGym(gym);
+
+        if (addGymResult.IsError)
+        {
+            return addGymResult.Errors;
+        }
+
+        await _subscriptionsRepository.UpdateAsync(subscription);
+        await _gymsRepository.AddGymAsync(gym);
+        await _unitOfWork.CommitChangesAsync();
+
+        return gym;
+    }
+}
